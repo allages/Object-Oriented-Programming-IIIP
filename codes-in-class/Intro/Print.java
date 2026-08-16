4c719457d27afc08aff8ae47f99d91208af30d67
allages <allslages@gmail.com>
2026-08-16 00:51:04 -0300

    Correções

--- codes-in-class/Intro/Print.java
+++ codes-in-class/Intro/Print.java
@@ -2,7 +2,7 @@ package Intro;
 import java.util.Scanner;
 
 public class Print {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
 
         // Testando input!
