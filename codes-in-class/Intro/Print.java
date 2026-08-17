10a63e5f23d9646e8cf2d71ffea17be47d5c5a37
allages <allslages@gmail.com>
2026-08-16 22:34:27 -0300

    Programa leitura idade

--- codes-in-class/Intro/Print.java
+++ codes-in-class/Intro/Print.java
@@ -12,6 +12,23 @@ public class Print {
         nome = input.nextLine();
         System.out.println("Seu nome é: " + nome);
 
+        int idade;
+        System.out.println("Digite sua idade: ");
+        idade = input.nextInt();
+        if (idade >= 18){
+            System.out.println(nome + "é maior de idade!");
+        } else if (idade >= 0){
+            System.out.println(nome + "é menor de idade!\nFaltam:");
+            int i;
+            for (i = 1; idade < 17; i += 1){
+                idade += 1;
+                System.out.println(i);
+            }
+            System.out.println(i + " anos para você fazer a sua CNH!");
+        } else {
+            System.out.println("Idade inválida!");
+        }
+
         input.close();
     }
 }
