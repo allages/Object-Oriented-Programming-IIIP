0196310758b369cdf137f15ee50cb16a7e56b0b0
Allan Lucas <allslages@gmail.com>
2026-08-18 13:43:55 +0000

    Testando comandos e funções

--- codes-in-class/Intro/Print.java
+++ codes-in-class/Intro/Print.java
@@ -1,11 +1,6 @@
 package Intro;
 import java.util.Scanner;
 
-public class Print {
-
-    public static String gerarCpf(){
-    
-}
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
