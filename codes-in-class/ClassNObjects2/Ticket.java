a55d6b36420fe29f17debe85fb7889b2cd3ec991
allages <allslages@gmail.com>
2026-08-19 19:00:41 -0300

    Tickets completo e explicado

--- codes-in-class/ClassNObjects2/Ticket.java
+++ codes-in-class/ClassNObjects2/Ticket.java
@@ -1,17 +1,25 @@
 package ClassNObjects2;
 
 public enum Ticket{
-    NORMAL(1.0),
-    MEIA_ENTRADA(0.5),
-    VIP(0.1);
+    NORMAL(1.0, "CODIGO PROMOCIONAL"), // São objetos/instâncias, é como se eu criasse 3 tickets diferentes. Acontece nos enums
+    MEIA_ENTRADA(0.5, "SEM CODIGO PROM"),
+    VIP(0.1, "CODIGO PROMOCIONAL");
     
-    double desconto;
+    double desconto; // Atributos
+    String codigo;
 
-    Ticket (double d){
-        this.desconto = d;
+    Ticket (double d, String codigo){ // Construtor, ele está associado aos atributos
+        this.desconto = d; // Aqui ele está "levando" o valor do enum lá em cima é associado a variável correspondente
+        this.codigo = codigo;
+    }
+
+    // Os getters são métodos que possibilitam que outras classes recebam o valor da variável sem poder modificar ele
+    public double getDesconto(){
+        return desconto;
+    }
+
+    public String getCodigo(){
+        return codigo;
     }
 }
 
-public double getDesconto(){
-    return desconto;
-}
